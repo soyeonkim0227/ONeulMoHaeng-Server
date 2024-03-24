0915fd60f8ac7be90086aa2ef01e28aecc5989de
soyeonkim0227 <soyeonkim0227@naver.com>
2024-03-24 23:51:40 +0900

    🗑 :: remove :: 플래너 파일 삭제

--- src/planner/planner.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PlannerService } from './planner.service';
-import { PlannerController } from './planner.controller';
-
-@Module({
-  providers: [PlannerService],
-  controllers: [PlannerController]
-})
-export class PlannerModule {}
