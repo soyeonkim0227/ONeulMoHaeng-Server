4f1f17a5c817f473d876a62b31ac92ca4afe28d5
soyeonkim0227 <soyeonkim0227@naver.com>
2024-06-20 02:58:29 +0900

    ✍ :: style & refactor :: 의존성 주입 및 예외처리 추가

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,16 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { DiaryRepository } from 'src/shared/repositories/diary.repository';
 import { LikeRepository } from 'src/shared/repositories/like.repository';
 
 @Injectable()
 export class LikeService {
   constructor(
     private readonly likeRepository: LikeRepository,
+    private readonly diaryRepository: DiaryRepository,
     private readonly authService: AuthService,
   ) {}
 
   async addOrCancleLike(accessToken: string, diaryId: number) {
     const { userId } = await this.authService.validateAccess(accessToken);
+
+    const thisDiary = await this.diaryRepository.findOneDiaryById(diaryId);
+    if (!thisDiary) throw new NotFoundException('존재하지 않는 다이어리');
+    if (!thisDiary.isShown && thisDiary.userId !== userId)
+      throw new ForbiddenException('비공개 다이어리는 좋아요 추가/삭제 불가');
 
     const like = await this.likeRepository.checkLike(userId, diaryId);
 
